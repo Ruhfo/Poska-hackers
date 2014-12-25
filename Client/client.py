6120645310e170de97e832e7e6687ecfce5aeb51
Ruhfo <andres.jogi96@gmail.com>
2014-12-25 12:12:53 +0200

    Updated functions for sending and receiving messages

--- Client/client.py
+++ Client/client.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 
 import socket
+import struct
 
+DEFAULT_LENGTH = 2048
+def receive_message(sock):
+    blength  = sock.recv(4)
+    
+    if (len(blength) > 0): 
+       blength = struct.unpack("I", blength)
+       length = blength[0]       
+
+       print("Length: ", length)         
+       print("BLength: ", blength)         
+ 
+       chunks = []
+       msgLength = 0
+       msg = ""
+       
+       while (msgLength<length):
+           chunk = sock.recv(min(length-msgLength, DEFAULT_LENGTH))
+           chunks.append(chunk)
+           msgLength+=len(chunk)
+
+       for item in chunks:
+           msg+=item.decode()
+
+       print(msg) 
+
+    else:
+        print("We have lost connection with the server")
+        sock.close   
+        return
+ 
 s = socket.socket()
 host = socket.gethostname()
 port = 12345
 
 s.connect((host, port))
-print(s.recv(1024))
+receive_message(s)
 s.close
--- Server/src/server.py
+++ Server/src/server.py
@@ -4,24 +4,33 @@ import socket
 import configuration
 import queue
 import threading
+import struct
 
-DEFAULT_LENGTH = 255
+DEFAULT_LENGTH = 2048
 
 def receive (c):
 #Function responsible for listening to one client
     while True:
-        length = c.recv(1)
-        if (len(length) > 0):
-            length = length.decode()
-            if (type(length) == int):
-                length = int(length)
-            else:
-                length = DEFAULT_LENGTH
-            msg = c.recv(length) #aquiring message itself
+
+        length = c.recv(4)
+
+        if (len(length) > 0): #Aquiring message length
+
+            length = struct.unpack("I", length) 
+
             
-            while (len(msg)<length):
-                msg += c.recv(length-len(msg))
-        
+            chunks = []
+            msgLength = 0
+            msg = ""
+ 
+            while (msgLength<length):
+                chunk = c.recv(min(length-msgLength, DEFAULT_LENGTH))
+                chunks.append(chunk)
+                msgLength += len(chunk)
+            
+            for item in chunks:
+                msg+=item.decode()
+             
             message = (c , msg)
             msgQueue.put(message) #Add msg to queue 
             print("Message: ", msg.decode())
@@ -30,6 +39,20 @@ def receive (c):
             clients.remove(c)
             c.close
             return
+
+def sendMessage (message, client):
+#This function is responsible for sending one message to a client
+    msg = message.encode() 
+    length = len(msg)
+    
+    blength = struct.pack("I", length) 
+ 
+    sent = 0
+    client.send(blength)
+
+    while (sent<length):
+        #Sending string until everything is sent
+        sent = client.send(msg[sent:]) 
 
 #Configuring initial parametres
 msgQueue = queue.Queue()
@@ -51,15 +74,15 @@ while True:
    
     print("New connection from ", addr, "Type", type(c) )
     message = "Welcome to "+name
-    msg = message.encode() 
     
-    c.send(msg)
-
+    sendMessage(message, c)
+#Starting new listener thread
     t = threading.Thread(target=receive(c))
     t.daemon= True
     threads.append(t)
     t.start()
 
+# Sending message to all the clients
     if msgQueue.qsize() > 0:
 
         for i in range(msgQueue.qsize()):
@@ -68,7 +91,7 @@ while True:
 
             for c in clients:
                 if (c != client):
-                    c.send(msg)
+                   sendMessage(msg, c) 
                 else:
                     continue
             msgQueue.task_done()
